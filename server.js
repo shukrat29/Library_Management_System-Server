ce6f31512e858630b197a1bba69fa818e3460572
Shukra Tamang <shukratamang.npl@gmail.com>
2024-12-04 15:19:24 +1100

    backend project setup

---
+++ server.js
@@ -0,0 +1,25 @@
+import express from "express";
+
+const app = express();
+
+const PORT = process.env.PORT || 8000;
+
+// middlewares
+import cors from "cors";
+import morgan from "morgan";
+app.use(cors());
+app.use(morgan("dev"));
+app.use(express.json());
+
+// Server status
+app.get("/", (req, res) => {
+  res.json({
+    message: "Server is alive",
+  });
+});
+
+app.listen(PORT, (error) => {
+  error
+    ? console.log(error)
+    : console.log("Server is running at http:/localhost:" + PORT);
+});
