149a4d6a1a1ae8d29510013549dae32a28e8f10b
Shukra Tamang <shukratamang.npl@gmail.com>
2024-12-08 18:59:21 +1100

    create userSchema and Model

---
+++ models/userSchema.js
@@ -0,0 +1,40 @@
+import { required, types } from "joi";
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    fName: {
+      type: String,
+      required: true,
+    },
+    lName: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      default: "user",
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    phone: {
+      type: String,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    refreshJWT: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const userModel = mongoose.model("User", userSchema);
+export default userModel;
