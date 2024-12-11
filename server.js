f1cd6fdbbdf9a64402de7e59cd665a0900e6de2d
Shukra Tamang <shukratamang.npl@gmail.com>
2024-12-11 21:28:49 +1100

    add errorHandler middleware

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const PORT = process.env.PORT || 8000;
 // middlewares
 import cors from "cors";
 import morgan from "morgan";
+import { responseClient } from "./middleware/responseClient.js";
 
 app.use(cors());
 app.use(morgan("dev"));
@@ -17,6 +18,12 @@ app.use(express.json());
 
 // Routes / api endpoints
 app.use("/api/v1/auth", authRouter);
+
+// server status
+app.get("/", (req, res) => {
+  const message = "Server is live";
+  responseClient(req, res, message);
+});
 
 app.use(errorHandler);
 
